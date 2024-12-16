ffa33dca3f4651559d7f8f78640bf02e675cfac2
Delaney <SeguinJDelaney@gmail.com>
2024-12-15 19:56:40 -0500

    Added starter iOS project

---
+++ Solitaire/AppDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.rootViewController?.view.backgroundColor = .white
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
